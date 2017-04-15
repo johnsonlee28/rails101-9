c02bdfe26f23f0e78f3cd0a3fe670e09637a2e86
Johnsonlee <johnsonlee28@gmail.com>
2017-04-15 22:22:12 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :groups do
     resources :posts
+
+    member do
+      post :join
+      post :quit
+    end
   end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
